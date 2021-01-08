c353d29585dba5f6c2aee7eb5c5129e400f4c6ef
jorgevillaverde-sitewhere <jorge.villaverde@sitewhere.com>
2021-01-08 16:15:31 -0300

    fix: rbac

--- controllers/sitewhere.io/instances_controller.go
+++ controllers/sitewhere.io/instances_controller.go
@@ -43,6 +43,13 @@ type SiteWhereInstanceReconciler struct {
 // +kubebuilder:rbac:groups=sitewhere.io,resources=instances,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=sitewhere.io,resources=instances/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=sitewhere.io,resources=instances/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebinding,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
--- controllers/sitewhere.io/sitewheremicroservice_controller.go
+++ controllers/sitewhere.io/sitewheremicroservice_controller.go
@@ -42,6 +42,8 @@ type SiteWhereMicroserviceReconciler struct {
 // +kubebuilder:rbac:groups=sitewhere.io,resources=microservices,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=sitewhere.io,resources=microservices/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=sitewhere.io,resources=microservices/finalizers,verbs=update
+// +kubebuilder:rbac:groups="apps",resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=services,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
